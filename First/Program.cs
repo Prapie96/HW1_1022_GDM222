8c1f451e3d09069f7f8119863847340b6eaafee2
Pongsapat_1022 <pongsapat.inta@kmutt.acth>
2023-02-07 18:45:42 +0700

    Add_Homework_Ex1 && Ex3

---
+++ First/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+    class Program
+    {
+          static double Calculatefuntion(double x)
+        {
+            if (x * x <= 4)
+                return 2;
+                
+            else
+                return 1 + Calculatefuntion(x - (4 * (Math.Abs(x) / x) ) );
+
+        }
+        static void Main(string[] args)
+        {   double x,output;
+            Console.Write("Input number x = ");
+            x = double.Parse(Console.ReadLine());
+
+            output = Calculatefuntion(x);
+            Console.WriteLine("f({0}) = {1}",x,output);
+        }
+    }
